8891ad8a15e98857d4b6ab6d5c440551225716f7
feifeiwo007 <498877586@qq.com>
2020-06-28 22:24:58 +0800

    test

---
+++ Week03/236.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or p==root or q==root:
+            return root
+        left=self.lowestCommonAncestor(root.left,p,q)
+        right=self.lowestCommonAncestor(root.right,p,q)
+        if not left: return right
+        if not right: return left
+        return root
